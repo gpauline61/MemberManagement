58d7409875864811b4d52cb815d63a5b89a4df2d
Ma. Pauline Gonzales <gpauline61@gmail.com>
2026-02-12 14:26:06 +0800

    Added a new Entity Branch

---
+++ MemberManagement.Domain/Entities/Branch.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MemberManagement.Domain.Entities
+{
+    public class Branch
+    {
+        public int BranchID { get; set; }
+        public string BranchName { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
